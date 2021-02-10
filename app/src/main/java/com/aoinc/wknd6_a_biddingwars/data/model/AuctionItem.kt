ad22cc1ab9db1c3ae5d30defe869e9f4d5a806c7
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-02-09 22:31:05 -0500

    Remove extra gradle plugin line

--- app/src/main/java/com/aoinc/wknd6_a_biddingwars/data/model/AuctionItem.kt
+++ app/src/main/java/com/aoinc/wknd6_a_biddingwars/data/model/AuctionItem.kt
@@ -1,9 +1,11 @@
 package com.aoinc.wknd6_a_biddingwars.data.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-data class AuctionItem(// User input data
+@Parcelize
+data class AuctionItem(
+    // User input data
     val photoUri: String = "",
     val name: String = "",
     val description: String = "",
@@ -17,45 +19,4 @@ data class AuctionItem(// User input data
 
     // Firebase data
     var idKey: String = ""
-)
-    : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readDouble(),
-        parcel.readString() ?: "",
-        parcel.readInt(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readString() ?: ""
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(photoUri)
-        parcel.writeString(name)
-        parcel.writeString(description)
-        parcel.writeString(seller)
-        parcel.writeDouble(currentBid)
-        parcel.writeString(lastBidder)
-        parcel.writeInt(numBids)
-        parcel.writeByte(if (isSold) 1 else 0)
-        parcel.writeString(idKey)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<AuctionItem> {
-        override fun createFromParcel(parcel: Parcel): AuctionItem {
-            return AuctionItem(parcel)
-        }
-
-        override fun newArray(size: Int): Array<AuctionItem?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+): Parcelable
